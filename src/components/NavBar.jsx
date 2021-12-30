37e47175c02357dca28446c7cbef9c4d61819b2a
Guillaume <guillaumegozillon@gmail.com>
2021-12-30 18:03:08 +0100

    Add shape + style CSS

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,62 @@
 import { NavLink } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 const NavBar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
+  const [largeur, setLargeur] = useState(window.innerWidth)
+
+  const toggleNavSmallScreen = () => setToggleMenu(!toggleMenu)
+  const closeToggle = () => setToggleMenu(false)
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setLargeur(window.innerWidth)
+
+      if (window.innerWidth > 800) {
+        setToggleMenu(false)
+      }
+    }
+    window.addEventListener('resize', changeWidth)
+    return () => {
+      window.removeEventListener('resize', changeWidth)
+    }
+  }, [])
+
   return (
     <nav>
-      <NavLink to='/'>HOME</NavLink>
-      <NavLink to='/about'>ABOUT</NavLink>
-      <div className='empty middle'></div>
-      <NavLink activeclassname='active' to='/services'>
-        SERVICES
-      </NavLink>
-      <NavLink activeclassname='active' to='/projects'>
-        PROJECTS
-      </NavLink>
-      <NavLink activeclassname='active' to='/contact'>
-        CONTACT
-      </NavLink>
+      {(toggleMenu || largeur > 800) && (
+        <>
+          <NavLink onClick={closeToggle} to='/'>
+            HOME
+          </NavLink>
+          <NavLink onClick={closeToggle} to='/about'>
+            ABOUT
+          </NavLink>
+          <div className='empty middle'></div>
+          <NavLink
+            onClick={closeToggle}
+            activeclassname='active'
+            to='/services'
+          >
+            SERVICES
+          </NavLink>
+          <NavLink
+            onClick={closeToggle}
+            activeclassname='active'
+            to='/projects'
+          >
+            PROJECTS
+          </NavLink>
+          <NavLink onClick={closeToggle} activeclassname='active' to='/contact'>
+            CONTACT
+          </NavLink>
+        </>
+      )}
+      {largeur < 800 && (
+        <button onClick={toggleNavSmallScreen} className='btn'>
+          BTN
+        </button>
+      )}
     </nav>
   )
 }
